3c14865b2bc2e21894285784ba543366dc3fa733
Flappizy <75231665+Flappizy@users.noreply.github.com>
2021-04-24 04:24:14 +0100

    week 3 solution

---
+++ AlgorithmFridays/NumberStartAndEnd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmFridays
+{
+    class NumberStartAndEnd
+    {
+        public static string GetNumberStartAndEnd(int[] numbers, int number)
+        {
+            if (numbers.Length == 0)
+            {
+                return "[-1, -1]";
+            }
+
+            //If array has a length of 1, it is already sorted so skip
+            if (numbers.Length != 1)
+            {
+                Array.Sort(numbers);
+            }
+
+            int? start = null;
+            int? end = null;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    start = i;
+                    break;
+                }
+            }
+
+            if (start == null)
+            {
+                return "[-1, -1]";
+            }
+
+            for (int i = numbers.Length - 1; i >= start; i--)
+            {
+                if (numbers[i] == number)
+                {
+                    end = i;
+                    break;
+                }
+            }
+
+            return $"[{start}, {end}]"; 
+        }
+    }
+}
